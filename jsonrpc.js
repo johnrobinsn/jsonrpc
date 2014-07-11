8d7c0ad6696c5c4ff07a56f6ed2606b375c52d0b
johnrobinsn <johnrobinsn@gmail.com>
2014-07-11 12:06:03 -0400

    Added basic support for async notifications

--- jsonrpc.js
+++ jsonrpc.js
@@ -11,39 +11,45 @@ jsonRPC = function(u) {
     this.outstanding = {};
 
     this.websocket.onopen = function(e) {
-		if (self.onOpen)
-	    	self.onOpen(e);
+        if (self.onOpen)
+            self.onOpen(e);
     }
     
     this.websocket.onclose = function(e) {
-		if (self.onClose)
-	    	self.onClose(e);
+        if (self.onClose)
+            self.onClose(e);
     }
     
     this.websocket.onerror = function(e) {
-		if (self.onError)
-	    	self.onError(e);
+        if (self.onError)
+            self.onError(e);
     }
     
     this.websocket.onmessage = function(e) {
-		var o = JSON.parse(e.data);
-		if (o) {
-	    	var f = self.outstanding[o.id];
-	    	if (f) {
-				f(o.error, o.result);
-				delete self.outstanding[o.id];
+        var o = JSON.parse(e.data);
+        if (o) {
+		    if (o.id != null) {
+		        var f = self.outstanding[o.id];
+                if (f) {
+                    f(o.error, o.result);
+                    delete self.outstanding[o.id];
+                }
+            }
+	    	else {
+	    	    if (self.onNotification)
+	    			self.onNotification(o.method, o.params);
 	    	}
-		}
+	    }
     }
     
     this.call = function(m, p, f) {
 		var id = self.id++;
 		var o = {
-	    	method: m,
+        method: m,
 	    	id: id,
 	    	params: p};
 		if (f) {
-	    	self.outstanding[id] = f;
+		    self.outstanding[id] = f;
 		}
 		self.websocket.send(JSON.stringify(o));
     }
