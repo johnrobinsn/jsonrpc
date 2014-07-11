b33abf17a8de32949284b73bc63045ef67ec6ad1
johnrobinsn <johnrobinsn@gmail.com>
2014-07-11 11:24:18 -0400

    Inital Add

---
+++ jsonrpc.js
@@ -0,0 +1,51 @@
+// John Robinson (c) 2009-2014
+// JSON RPC Over Websockets
+// MIT License
+
+jsonRPC = function(u) {
+    var self = this;
+    this.id = 1;
+    this.url = u;
+    this.websocket = new WebSocket(this.url);
+    this.outstanding = {};
+
+    this.websocket.onopen = function(e) {
+		if (self.onOpen)
+	    	self.onOpen(e);
+    }
+    
+    this.websocket.onclose = function(e) {
+		if (self.onClose)
+	    	self.onClose(e);
+    }
+    
+    this.websocket.onerror = function(e) {
+		if (self.onError)
+	    	self.onError(e);
+    }
+    
+    this.websocket.onmessage = function(e) {
+		var o = JSON.parse(e.data);
+		if (o) {
+	    	var f = self.outstanding[o.id];
+	    	if (f) {
+				f(o.error, o.result);
+				delete self.outstanding[o.id];
+	    	}
+		}
+    }
+    
+    this.call = function(m, p, f) {
+		var id = self.id++;
+		var o = {
+	    	method: m,
+	    	id: id,
+	    	params: p};
+		if (f) {
+	    	self.outstanding[id] = f;
+		}
+		self.websocket.send(JSON.stringify(o));
+    }
+    
+    return this;
+}
